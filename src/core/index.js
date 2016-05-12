02df87c9a20c7f8fb528564fccda503738462cae
杨佳军 <yangjj@chezhil.com>
2016-05-12 15:27:14 +0800

    添加 index 主入口 注释

--- src/core/index.js
+++ src/core/index.js
@@ -1,8 +1,17 @@
+/**
+ * 主入口文件 
+ * import Vue from 'vue'
+ * 就是引入的这个文件的vue
+ */
 import Vue from './instance/index'
+
+/**
+ * vue项目内部使用的api
+ */
 import { initGlobalAPI } from './global-api/index'
 
-initGlobalAPI(Vue)
+initGlobalAPI(Vue)  //把这个API给vue
 
-Vue.version = '2.0.0-alpha.0'
+Vue.version = '2.0.0-alpha.0' //vue版本号
 
-export default Vue
+export default Vue  //导出Vue
--- src/core/instance/index.js
+++ src/core/instance/index.js
@@ -1,23 +1,36 @@
-import { initProxy } from './proxy'
-import { initState, stateMixin } from './state'
-import { initRender, renderMixin } from './render'
-import { initEvents, eventsMixin } from './events'
-import { initLifecycle, lifecycleMixin, callHook } from './lifecycle'
-import { mergeOptions } from '../util/index'
+import { initProxy } from './proxy' //目前不知道干嘛用
+import { initState, stateMixin } from './state' //和数据相关
+import { initRender, renderMixin } from './render' //和渲染相关
+import { initEvents, eventsMixin } from './events' //和事件相关
+import { initLifecycle, lifecycleMixin, callHook } from './lifecycle' //和生命周期相关
+import { mergeOptions } from '../util/index' //合并父级options到子级的工具方法
 
-let uid = 0
+let uid = 0 //vue内部使用的编号,每创建一个vue实例就+1
 
+/**
+ * 导出Vue
+ */
 export default class Vue {
+  /**
+   * 构造函数 new Vue() 或者new 组件 的时候执行
+   * options = {el,data,template...}
+   * 
+   */
   constructor (options) {
     this._init(options)
   }
 
+  /**
+   * 初始化Vue实例方法
+   * @param options
+   * @private
+   */
   _init (options) {
-    // a uid
-    this._uid = uid++
-    // a flag to avoid this being observed
-    this._isVue = true
-    // merge options
+    // a uid 
+    this._uid = uid++ 
+    // a flag to avoid this being observed 标记是Vue对象
+    this._isVue = true 
+    // merge options 合并options
     this.$options = mergeOptions(
       this.constructor.options,
       options || {},
@@ -28,16 +41,40 @@ export default class Vue {
     } else {
       this._renderProxy = this
     }
-    initLifecycle(this)
-    initEvents(this)
-    callHook(this, 'init')
-    initState(this)
-    callHook(this, 'created')
-    initRender(this)
+    initLifecycle(this) //初始化生命周期
+    initEvents(this) //初始化事件
+    callHook(this, 'init') //调用init钩子
+    initState(this) //初始化数据
+    callHook(this, 'created') //调用created钩子
+    initRender(this) //执行渲染
   }
 }
-
+/**
+ * 给Vue.prototype添加数据方法
+ * 1.$data 可以用这个属性操作数据
+ * 2.$watch 方法
+ */
 stateMixin(Vue)
+
+/**
+ * 给Vue.prototype添加事件方法  当你使用 this.$emit('事件名称',参数) 就是这里添加的方法
+ * 1.$on
+ * 2.$off
+ * 3.$once
+ * 4.$emit
+ */
 eventsMixin(Vue)
+
+/**
+ * 给Vue.prototype添加生命周期方法 _开头的是内部方法 $开头的是公开方法 
+ * 1.$forceUpdate 使vm强制更新,如你修改了一个值,要让他马上反应到视图上就可以用 this.$forceUpdate() 方法
+ * 2.$destroy 销毁当前vm
+ */
 lifecycleMixin(Vue)
+
+/**
+ * 给Vue.prototype添加渲染方法
+ * 1.$nextTick 数据改变要显示在视图之后执行的操作,可以使用这个方法
+ * 2.$isServer 2.0 新增加的,服务端渲染相关
+ */
 renderMixin(Vue)
